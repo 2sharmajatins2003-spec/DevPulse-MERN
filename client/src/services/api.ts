d69bd7e0a308ede0a374acbde6c9fe7fd418d7f2
2sharmajatins2003-spec <2sharmajatin.s2003@gmail.com>
2026-07-08 22:48:42 +0530

    commit

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://devpulse-backend-2s2c.onrender.com/api",
+    "https://devpulse-backend-p950.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
